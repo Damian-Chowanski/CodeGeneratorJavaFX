60bc8b8dbd4887ce08c86e9866c38fdc0cf19283
Damian-Chowanski <dchowanski30@gmail.com>
2023-10-16 19:43:33 +0200

    added button to switch scene

--- src/main/java/com/example/codegeneratorfx/CodeGeneratorFX.java
+++ src/main/java/com/example/codegeneratorfx/CodeGeneratorFX.java
@@ -11,8 +11,8 @@ public class CodeGeneratorFX extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CodeGeneratorFX.class.getResource("start-scene.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Welcome in Code Generator!");
         stage.setScene(scene);
         stage.show();
     }
@@ -20,4 +20,5 @@ public class CodeGeneratorFX extends Application {
     public static void main(String[] args) {
         launch();
     }
+
 }
--- src/main/java/com/example/codegeneratorfx/CodeGeneratorFXController.java
+++ src/main/java/com/example/codegeneratorfx/CodeGeneratorFXController.java
@@ -1,11 +1,26 @@
 package com.example.codegeneratorfx;
 
-public class CodeGeneratorFXController {
-    /*@FXML
-    private Label welcomeText;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class CodeGeneratorFXController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }*/
+    public void onButtonClick(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("administration-panel.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setTitle("Let's generate some codes!");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
