542414494b86ff3ebbdee3a554d2db3404c2e5fc
Damian-Chowanski <dchowanski30@gmail.com>
2023-10-16 20:23:58 +0200

    created method to generate array list of codes

--- src/main/java/com/example/codegeneratorfx/supportClasses/Lottery.java
+++ src/main/java/com/example/codegeneratorfx/supportClasses/Lottery.java
@@ -1,6 +1,7 @@
 package com.example.codegeneratorfx.supportClasses;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Lottery {
     private ArrayList<Code> codes;
@@ -17,14 +18,37 @@ public class Lottery {
         this.codes = codes;
     }
 
-    public void generateCodes(){
+    public void generateCodes(int numberOfCodes, int codeLength){
+        Random rand = new Random();
         //Random class with chars equals to {a...z} or {A...Z} or {0...9}
         RandomInRanges rir = new RandomInRanges(48, 57);
         rir.addRange(65, 90);
         rir.addRange(97, 122);
 
+        //Set win and use chance percent:
+        double usedChance = 0.45;
+        double winChance = 0.05;
 
+        //Generates unique codes to ArrayList<String>
 
+        for (int i = 0; i < numberOfCodes; i++) {
+            String code = "";
+            for (int j = 0; j < codeLength; j++) {
+                code += (char) rir.getRandom();
+            }
+            if (!isUnique(code)) {
+                i--;
+            } else codes.add(new Code(code,(rand.nextDouble() < usedChance),rand.nextDouble() > winChance));
+        }
+    }
+
+    private boolean isUnique(String codeToCheck) {
+        for (Code code: codes){
+            if (code.code.equals(codeToCheck)){
+                return false;
+            }
+        }
+        return true;
     }
 
 
