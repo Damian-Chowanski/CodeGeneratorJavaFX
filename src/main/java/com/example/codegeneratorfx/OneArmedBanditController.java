54238695010b1c84f057e03a138f1dc3fb4b92f3
Damian-Chowanski <dchowanski30@gmail.com>
2023-10-30 19:10:05 +0100

    fixed game controller

--- src/main/java/com/example/codegeneratorfx/OneArmedBanditController.java
+++ src/main/java/com/example/codegeneratorfx/OneArmedBanditController.java
@@ -32,6 +32,13 @@ public class OneArmedBanditController {
     private Label code3_lbl;
 
     @FXML
+    private Label code1Won_lbl;
+    @FXML
+    private Label code2Won_lbl;
+    @FXML
+    private Label code3Won_lbl;
+
+    @FXML
     private Button spinBtn;
 
     @FXML
@@ -66,56 +73,72 @@ public class OneArmedBanditController {
         code3_lbl.setVisible(false);
         win_lbl.setVisible(false);
         lose_lbl.setVisible(false);
-        if (!(playingCodes.size() <3)) {
+        drawnCodes.clear();
+
+        if (!(playingCodes.size() <3) && cash >=50) {
+            cash -= 50;
+            cash_lbl.setText("cash: " + cash);
+            spinBtn.setText("Spin Again!");
 
             int maxSteps = 10;
-            Timeline timeline1 = startTimeLine(code1_lbl, maxSteps);
-            Timeline timeline2 = startTimeLine(code2_lbl, maxSteps);
-            Timeline timeline3 = startTimeLine(code3_lbl, maxSteps);
+            Timeline timeline1 = startTimeLine(code1_lbl,code1Won_lbl, maxSteps);
+            Timeline timeline2 = startTimeLine(code2_lbl,code2Won_lbl, maxSteps);
+            Timeline timeline3 = startTimeLine(code3_lbl,code3Won_lbl, maxSteps);
 
             timeline1.setOnFinished(TimeEvent -> {
+                addCodeToDrawn(code1_lbl);
                 removeSelectedCodeFromList(code1_lbl);
                 timeline2.play();
             });
 
             timeline2.setOnFinished(TimeEvent -> {
+                addCodeToDrawn(code2_lbl);
                 removeSelectedCodeFromList(code2_lbl);
                 timeline3.play();
             });
 
             timeline3.setOnFinished(TimeEvent -> {
+                addCodeToDrawn(code3_lbl);
+                System.out.println(drawnCodes.size());
                 if (isWon()) {
                     win_lbl.setVisible(true);
                     cash += 100;
                     cash_lbl.setText("cash: " + cash);
-                    spinBtn.setText("Spin Again!");
                 } else {
                     lose_lbl.setVisible(true);
-                    cash -= 100;
+                    cash -= 50;
                     cash_lbl.setText("cash: " + cash);
-                    spinBtn.setText("Spin Again!");
                 }
+                removeSelectedCodeFromList(code3_lbl);
             });
 
             timeline1.play();
         }else goToMainMenu(event);
     }
 
+    private void addCodeToDrawn(Label label) {
+        for (Code code: playingCodes){
+            if(code.getCode().equalsIgnoreCase(label.getText())){
+                drawnCodes.add(code);
+            }
+        }
+    }
+
     private boolean isWon() {
         for (Code code: drawnCodes){
-            if (!(code.getIsWinning()) || code.getIsUsed()){
+            if (!(code.getIsWinning())){
                 return false;
             }
         }
         return true;
     }
 
-    private Timeline startTimeLine(Label label, int maxSteps) {
+    private Timeline startTimeLine(Label label, Label labelWon, int maxSteps) {
         final int[] currentStep = {0};
         Timeline timeline = new Timeline(
                 new KeyFrame(Duration.seconds(0.15), event -> {
                     if (currentStep[0] < maxSteps) {
-                        spinLabel(label);
+                        spinLabel(label, labelWon);
                         currentStep[0]++;
                     }
                 })
@@ -126,19 +149,20 @@ public class OneArmedBanditController {
 
     private void removeSelectedCodeFromList(Label label) {
         ArrayList<Code> copyList = new ArrayList<>(playingCodes);
-        for (Code code:playingCodes){
-            if(code.getCode().equals(label.getText())){
-                drawnCodes.add(code);
+        for (Code code: playingCodes){
+            if(code.getCode().equalsIgnoreCase(label.getText())){
                 copyList.remove(code);
             }
         }
         playingCodes = copyList;
     }
 
-    private void spinLabel(Label label) {
+    private void spinLabel(Label label, Label labelWon) {
         Random rand = new Random();
+        int randomID = rand.nextInt(0, playingCodes.size());
         label.setVisible(true);
-        label.setText(String.valueOf(playingCodes.get(rand.nextInt(0, playingCodes.size())).getCode()));
+        label.setText(String.valueOf(playingCodes.get(randomID).getCode()));
+        labelWon.setText(String.valueOf(playingCodes.get(randomID).getIsWinning()));
     }
 
     @FXML
